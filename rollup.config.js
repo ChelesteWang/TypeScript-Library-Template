983679f0c10542ec21c77c092a7d7ea5cf74b883
yuqing521 <1581446178@qq.com>
2021-09-25 18:14:13 +0800

    feat: add sourcemap

--- rollup.config.js
+++ rollup.config.js
@@ -1,16 +1,13 @@
 import resolve from '@rollup/plugin-node-resolve'
 import commonjs from '@rollup/plugin-commonjs'
 import typescript from '@rollup/plugin-typescript'
+import sourcemap from 'rollup-plugin-sourcemaps'
 
 export default {
-  input: 'src/index.ts', 
+  input: 'src/index.ts',
   output: {
-    file: 'dist/index.js', 
-    format: 'module' 
+    file: 'dist/index.esm.js',
+    format: 'module'
   },
-  plugins: [
-    resolve(), 
-    commonjs(), 
-    typescript() 
-  ]
+  plugins: [resolve(), commonjs(), typescript(), sourcemap()]
 }
