8b07b1c08924afba4821895e391a853c7f406fb3
yuqing521 <1581446178@qq.com>
2021-09-28 09:12:06 +0800

    feat: add terser plugin

--- rollup.config.js
+++ rollup.config.js
@@ -2,6 +2,7 @@ import resolve from '@rollup/plugin-node-resolve'
 import commonjs from '@rollup/plugin-commonjs'
 import typescript from '@rollup/plugin-typescript'
 import sourceMaps from 'rollup-plugin-sourcemaps'
+import { terser } from 'rollup-plugin-terser'
 
 const pkg = require('./package.json')
 
@@ -9,8 +10,8 @@ export default {
   input: 'src/index.ts',
   output: {
     file: pkg.main,
-    format: 'module',
+    format: 'esm',
     sourcemap: true
   },
-  plugins: [resolve(), commonjs(), typescript(), sourceMaps()]
+  plugins: [resolve(), commonjs(), typescript(), sourceMaps(), terser()]
 }
